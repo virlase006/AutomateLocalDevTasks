121001497fdf0e31d4c525bebb0261f062867f6c
Virlla Devi Soothar <vds@stylelabs.com>
2019-11-01 14:37:48 -0400

    First draft

---
+++ MLocalRun/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace MLocalRun
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_gitVersion = new System.Windows.Forms.TextBox();
+            this.txt_gitUsername = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.check_DoYouHaveGit = new System.Windows.Forms.CheckBox();
+            this.lbl_chooseGit = new System.Windows.Forms.Label();
+            this.btn_GitRepoBrowse = new System.Windows.Forms.Button();
+            this.txt_gitRepoPath = new System.Windows.Forms.TextBox();
+            this.panel_GitLogin = new System.Windows.Forms.Panel();
+            this.lbl_repoError = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.panel_GitLogin.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Git connection";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Git version";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(40, 58);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Username";
+            // 
+            // txt_gitVersion
+            // 
+            this.txt_gitVersion.Location = new System.Drawing.Point(113, 22);
+            this.txt_gitVersion.Name = "txt_gitVersion";
+            this.txt_gitVersion.Size = new System.Drawing.Size(100, 20);
+            this.txt_gitVersion.TabIndex = 8;
+            // 
+            // txt_gitUsername
+            // 
+            this.txt_gitUsername.Location = new System.Drawing.Point(113, 51);
+            this.txt_gitUsername.Name = "txt_gitUsername";
+            this.txt_gitUsername.Size = new System.Drawing.Size(100, 20);
+            this.txt_gitUsername.TabIndex = 9;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(246, 58);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Next";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // check_DoYouHaveGit
+            // 
+            this.check_DoYouHaveGit.AutoSize = true;
+            this.check_DoYouHaveGit.Location = new System.Drawing.Point(27, 57);
+            this.check_DoYouHaveGit.Name = "check_DoYouHaveGit";
+            this.check_DoYouHaveGit.Size = new System.Drawing.Size(146, 17);
+            this.check_DoYouHaveGit.TabIndex = 11;
+            this.check_DoYouHaveGit.Text = "Do you have a git repo ? ";
+            this.check_DoYouHaveGit.UseVisualStyleBackColor = true;
+            this.check_DoYouHaveGit.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // lbl_chooseGit
+            // 
+            this.lbl_chooseGit.AutoSize = true;
+            this.lbl_chooseGit.Location = new System.Drawing.Point(27, 97);
+            this.lbl_chooseGit.Name = "lbl_chooseGit";
+            this.lbl_chooseGit.Size = new System.Drawing.Size(110, 13);
+            this.lbl_chooseGit.TabIndex = 12;
+            this.lbl_chooseGit.Text = "Choose git repo folder";
+            // 
+            // btn_GitRepoBrowse
+            // 
+            this.btn_GitRepoBrowse.Location = new System.Drawing.Point(357, 87);
+            this.btn_GitRepoBrowse.Name = "btn_GitRepoBrowse";
+            this.btn_GitRepoBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btn_GitRepoBrowse.TabIndex = 13;
+            this.btn_GitRepoBrowse.Text = "Browse";
+            this.btn_GitRepoBrowse.UseVisualStyleBackColor = true;
+            this.btn_GitRepoBrowse.Click += new System.EventHandler(this.btn_GitRepoBrowse_Click);
+            // 
+            // txt_gitRepoPath
+            // 
+            this.txt_gitRepoPath.Location = new System.Drawing.Point(180, 90);
+            this.txt_gitRepoPath.Name = "txt_gitRepoPath";
+            this.txt_gitRepoPath.Size = new System.Drawing.Size(154, 20);
+            this.txt_gitRepoPath.TabIndex = 14;
+            this.txt_gitRepoPath.TextChanged += new System.EventHandler(this.txt_gitRepoPath_TextChanged);
+            // 
+            // panel_GitLogin
+            // 
+            this.panel_GitLogin.Controls.Add(this.txt_gitVersion);
+            this.panel_GitLogin.Controls.Add(this.label4);
+            this.panel_GitLogin.Controls.Add(this.label5);
+            this.panel_GitLogin.Controls.Add(this.txt_gitUsername);
+            this.panel_GitLogin.Controls.Add(this.button2);
+            this.panel_GitLogin.Location = new System.Drawing.Point(27, 292);
+            this.panel_GitLogin.Name = "panel_GitLogin";
+            this.panel_GitLogin.Size = new System.Drawing.Size(578, 100);
+            this.panel_GitLogin.TabIndex = 15;
+            // 
+            // lbl_repoError
+            // 
+            this.lbl_repoError.AutoSize = true;
+            this.lbl_repoError.ForeColor = System.Drawing.Color.Red;
+            this.lbl_repoError.Location = new System.Drawing.Point(180, 117);
+            this.lbl_repoError.Name = "lbl_repoError";
+            this.lbl_repoError.Size = new System.Drawing.Size(35, 13);
+            this.lbl_repoError.TabIndex = 16;
+            this.lbl_repoError.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(669, 427);
+            this.Controls.Add(this.lbl_repoError);
+            this.Controls.Add(this.panel_GitLogin);
+            this.Controls.Add(this.txt_gitRepoPath);
+            this.Controls.Add(this.btn_GitRepoBrowse);
+            this.Controls.Add(this.lbl_chooseGit);
+            this.Controls.Add(this.check_DoYouHaveGit);
+            this.Controls.Add(this.label3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.panel_GitLogin.ResumeLayout(false);
+            this.panel_GitLogin.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_gitVersion;
+        private System.Windows.Forms.TextBox txt_gitUsername;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.CheckBox check_DoYouHaveGit;
+        private System.Windows.Forms.Label lbl_chooseGit;
+        private System.Windows.Forms.TextBox txt_gitRepoPath;
+        private System.Windows.Forms.Button btn_GitRepoBrowse;
+        private System.Windows.Forms.Panel panel_GitLogin;
+        private System.Windows.Forms.Label lbl_repoError;
+    }
+}
+
---
+++ MLocalRun/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Management.Automation;
+using System.Collections.ObjectModel;
+
+namespace MLocalRun
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            using (PowerShell PowerShellInstance = PowerShell.Create())
+            {
+                // use "AddScript" to add the contents of a script file to the end of the execution pipeline.
+                // use "AddCommand" to add individual commands/cmdlets to the end of the execution pipeline.
+                var script = new StreamReader(@"C:\Users\adv\Downloads\GetGitRepo.ps1");
+
+
+                string newScript = script.ReadToEnd();
+                  //  File.ReadAllText(@"C:\Users\adv\Downloads\GetGitRepo.ps1");
+                PowerShellInstance.AddScript(newScript);
+
+                // use "AddParameter" to add a single parameter to the last command/script on the pipeline.
+                PowerShellInstance.AddParameter("USERNAME", txt_gitUsername.Text);
+                PowerShellInstance.AddParameter("PASSWORD", "");
+                PowerShellInstance.AddParameter("STYLELABSDIR", txt_gitRepoPath.Text);
+                PowerShellInstance.AddParameter("VERSION", txt_gitVersion.Text);
+                PowerShellInstance.AddParameter("RepoExist", Convert.ToInt32(check_DoYouHaveGit.Checked));
+
+                Collection<PSObject> PSOutput = PowerShellInstance.Invoke();
+               
+                foreach (PSObject outputItem in PSOutput)
+                {
+                    // if null object was dumped to the pipeline during the script then a null
+                    // object may be present here. check for null to prevent potential NRE.
+                    if (outputItem != null)
+                    {
+                       
+                        Console.WriteLine(outputItem.BaseObject.ToString() + "\n"); 
+
+                    }
+                }          
+                
+            }
+          
+
+            //var ps1File = @"C:\Users\adv\Downloads\GetGitRepo.ps1";
+            //var filename = "C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe";
+            //var arguments = $"-NoProfile -ExecutionPolicy unrestricted -FILE \"{ps1File}\" advandizda  \"\"  C:\\Users\\adv\\Documents\\PowershellTest\\stylelabs.m release\\3.1.3 1";
+
+
+            //var startInfo = new ProcessStartInfo()
+            //{
+            //    FileName = filename,
+            //    Arguments = arguments,
+            //    UseShellExecute = true
+            //};
+
+            //var proc = Process.Start(startInfo);
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (check_DoYouHaveGit.Checked)
+            {
+                lbl_chooseGit.Text = "Choose existing git repo folder.";
+            }
+            else
+            {
+                lbl_chooseGit.Text = "Choose where you CLONE git repo. ";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lbl_repoError.Visible = false;
+            check_DoYouHaveGit.Checked = true;
+            txt_gitUsername.Parent = panel_GitLogin;
+            txt_gitVersion.Parent = panel_GitLogin;
+            var filePath = txt_gitRepoPath.Text;
+            if (filePath == "" || filePath == null)
+            {
+                panel_GitLogin.Hide();
+            }
+            else
+            {
+                panel_GitLogin.Show();
+            }
+        }
+
+        private void btn_GitRepoBrowse_Click(object sender, EventArgs e)
+        {
+
+            FolderBrowserDialog ofd = new FolderBrowserDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                string sourceFile = ofd.SelectedPath;
+                txt_gitRepoPath.Text = sourceFile;
+
+            }
+        }
+
+        private void txt_gitRepoPath_TextChanged(object sender, EventArgs e)
+        {
+
+            if (!String.IsNullOrEmpty(txt_gitRepoPath.Text))
+            {
+                if (Directory.Exists(txt_gitRepoPath.Text))
+                {
+                    panel_GitLogin.Show();
+                    lbl_repoError.Visible = false;
+                }
+                else
+                {
+                    panel_GitLogin.Hide();
+                    lbl_repoError.Text = "Invalid path !!!";
+                    lbl_repoError.Visible = true;
+                }
+            }
+        }
+    }
+}
